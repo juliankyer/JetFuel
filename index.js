85a9ecef118f116c16746f8335d2a386a76f97f7
Kyle Zucker <zkyle12@gmail.com>
2017-05-01 14:45:04 -0600

    Add base HTMLd

---
+++ index.js
@@ -0,0 +1,4 @@
+
+const addNewFolder = () => {
+  $('#link-folder-dropdown').append(`<option>${$('#new-folder-name').val()}</option>`)
+}
---
+++ server.js
@@ -0,0 +1,13 @@
+//
+// Need to store per link:
+//  Long URL - only thing we get to start
+//  Short URL
+//  Number of clicks
+//  Date added
+//  Folder  
+//
+//
+//
+//
+//
+//
