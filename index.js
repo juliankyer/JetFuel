3a0a31234af74c76d50871d5fa2d2456e57b260a
Julian Kyer <julian.kyer@gmail.com>
2017-05-02 14:34:12 -0600

    Fix on click for add folder button

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
-const addNewFolder = (e) => {
+$('#add-folder-btn').on('click', function(e) {
   e.preventDefault();
   console.log('boom');
   $('#link-folder-dropdown').append(`<option value="${$('#new-folder-name').val()}">${$('#new-folder-name').val()}</option>`)
-}
+})
