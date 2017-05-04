acb463debc24fff2c848792e0fc7f7b470459957
Julian Kyer <julian.kyer@gmail.com>
2017-05-04 17:13:37 -0600

    Increment clicks functionality

--- public/index.js
+++ public/index.js
@@ -1,42 +1,33 @@
 
 const addFolder = () => {
-  const newFolderName = $('#new-folder-name').val()
-
+  const newFolderName = $('#new-folder-name').val();
   const currentOptions = $('#link-folder-dropdown').children().toArray().map(item => {
     return item.innerText
-  })
+  });
 
   if(currentOptions.indexOf(newFolderName) < 0) {
-    console.log('in the if');
 
     fetch('/api/v1/folders', {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(
-        {
+      body: JSON.stringify({
           name: $('#new-folder-name').val(),
-          // folderID
-        }
-      )
+        })
     }).then((response) => {
       response.json().then((data) => {
         console.log(data);
         $('#link-folder-dropdown').append(`<option value=${data.id}>${data.name}</option>`);
-
       })
     }).catch(err => console.log(err))
   } else {
-    console.log('Folder already made up yo!');
     //come back to this
   }
 }
 
 const addURL = () => {
   const newURL = $('#long-url-input').val();
-  //grab option value
   const folderID = $('#link-folder-dropdown').val();
   //validation?
-  console.log(newURL, folderID);
   fetch('/api/v1/links', {
     method: "POST",
     headers: { "Content-Type": "application/json"},
@@ -46,42 +37,21 @@ const addURL = () => {
     })
   }).then((response) => {
     response.json().then((json) => {
-      console.log(json[0]);
       const linkID = json[0]
     })
-    ////
   })
 }
 
 /////HELPERS HERE! @#$%@#$%@#$%@#$%@$%@$%@#$%@#$%@#$5
 const convertIdToUrl = (id) => {
-  const urlBase = 'http://localhost:3000/'
-  return urlBase + id.toString(32)
+  const urlBase = 'http://localhost:3000/';
+  return urlBase + id.toString(32);
 }
 
 const refreshLinkDisplay = (folderID) => {
-
-}
-
-$('#add-folder-btn').on('click', function(e) {
-  e.preventDefault();
-  addFolder();
-});
-
-$('#add-url').on('click', function(e) {
-  e.preventDefault();
-  addURL();
-})
-
-$('#link-folder-dropdown').on('change', function(e)  {
-  const folderID = e.target.value
-  console.log(folderID)
-
   fetch(`/api/v1/folders/${folderID}`)
     .then((response) => {
-      console.log(response)
       response.json().then((json) => {
-        console.log(json)
         $('.link-wrapper').empty()
         json.forEach((link) => {
           $('.link-wrapper').prepend(`
@@ -97,25 +67,41 @@ $('#link-folder-dropdown').on('change', function(e)  {
         })
       })
     })
+};
+
+$('#add-folder-btn').on('click', function(e) {
+  e.preventDefault();
+  addFolder();
+});
+
+$('#add-url').on('click', function(e) {
+  const folderID = $('#link-folder-dropdown').val();
+  e.preventDefault();
+  addURL();
+  refreshLinkDisplay(folderID);
 })
 
+$('#link-folder-dropdown').on('change', function(e)  {
+  const folderID = e.target.value
+  refreshLinkDisplay(folderID);
+});
+
 const populateFolderOptions = (options) => {
-  console.log('in the pop', options)
-  const select = $('#link-folder-dropdown')
+  const select = $('#link-folder-dropdown');
   options.forEach((folder) => {
     select.append(`<option value=${folder.id}>${folder.name}</option>`)
-  })
+  });
 }
 
 $(document).ready(() => {
-  console.log('doc is read')
   fetch('/api/v1/folders')
     .then((response) => {
-      console.log(response)
-      response.json().then(json => {
+      response.json()
+      .then(json => {
         populateFolderOptions(json)
       })
-    })
-})
+    });
+});
+
 
 
