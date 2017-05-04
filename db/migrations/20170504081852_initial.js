9b0fae805189bf665779241d9e7b40dce8a01190
Kyle Zucker <zkyle12@gmail.com>
2017-05-04 17:48:31 -0600

    update DB migration

--- db/migrations/20170504081852_initial.js
+++ db/migrations/20170504081852_initial.js
@@ -4,28 +4,27 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('folders', function(table) {
       table.increments('id').primary();
       table.string('name');
-      
-      table.timestamps();
+
+      table.timestamps(true, true);
     }),
-    
+
     knex.schema.createTable('links', function(table) {
       table.increments('id').primary();
       table.string('longURL');
-      table.string('shortURL');
       table.integer('clicks');
-      
+
       table.integer('folder_id').unsigned();
       table.foreign('folder_id').references('folders.id');
-      
-      table.timestamps();
+
+      table.timestamps(true, true);
     })
   ])
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('folders'),
-    knex.schema.dropTable('links')
+    knex.schema.dropTable('links'),
+    knex.schema.dropTable('folders')
   ]);
 };
 
--- db/seeds/dev/folders.js
+++ db/seeds/dev/folders.js
@@ -12,13 +12,11 @@ exports.seed = function(knex, Promise) {
           return knex('links').insert([
             {
               longURL: 'www.google.com',
-              shortURL: 'jet.fuel/2343d',
               clicks: 0,
               folder_id: folder[0]
             },
             {
               longURL: 'www.cyclingnews.com',
-              shortURL: 'jet.fuel/2d33d',
               clicks: 0,
               folder_id: folder[0]
             }
