50f5f08dc9a0e12ffd2770369644770baf5369cc
Julian Kyer <julian.kyer@gmail.com>
2017-05-05 10:20:25 -0600

    Heroku setup

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 
-const environment = 'development';
+const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration)
 
