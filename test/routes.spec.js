9c9e26efd2d1b1ff5bb419a1ff9b6d25b001ec6f
Julian Kyer <julian.kyer@gmail.com>
2017-05-05 02:12:35 -0600

    Test suite setup continued

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,5 +1,3 @@
-//npm install -D mocha chai chai-http
-
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
@@ -14,12 +12,13 @@ describe('Client routes', () => {
       .end((error, response) => {
         response.should.have.status(200);
         response.should.be.html;
+        console.log(response);
         //check for html elements of homepage
         done();
       });
   });
   
-  it('should return a 404 for a non-existent route', (done) => {
+  it.skip('should return a 404 for a non-existent route', (done) => {
     chai.request(server)
       .get('/nope')
       .end((error, response) => {
@@ -30,51 +29,53 @@ describe('Client routes', () => {
   });
 });
 
-describe('API routes', () => {
-  before((done) => {
-    
-    done();
-  });
+// describe('API routes', () => {
+//   before((done) => {
+//     
+//     done();
+//   });
+//   
+//   afterEach((done) => {
+//     
+//     done();
+//   });
+//   
+//   describe('GET /api/v1/folders', () => {
+//     it('should return all of the folders', (done) => {
+//       chai.request(server)
+//         .get('/api/v1/students')
+//         .end((error, response) => {
+//           response.shouldhave.status(200);
+//           
+//           done();
+//         });
+//     });
+//   });
+//   
+//   describe('POST /api/v1/folders', () => {
+//     it('should create a new folder', (done) => {
+//       
+//     });
+//     
+//     it('should not create a new folder if that folder already exists', () => {
+//       
+//     });    
+//   });
+//   
+//   describe('POST /api/v1/links', () => {
+//     it('should create a new link', () => {
+//       
+//     });
+//     
+//     it('should not create a new link with missing data', () => {
+//       
+//     });
+//   });
+//   
+//   describe('click events', () => {
+//     it('should handle some clicks', () => {
+//       
+//     });
+//   });
   
-  afterEach((done) => {
-    
-    done();
-  });
-  
-  describe('GET /api/v1/folders', () => {
-    it('should return all of the folders', (done) => {
-      chai.request(server)
-        .get('/api/v1/students')
-        .end((error, response) => {
-          response.shouldhave.status(200);
-          
-          done();
-        });
-    });
-  });
-  
-  describe('POST /api/v1/folders', () => {
-    it('should create a new folder', (done) => {
-      
-    });
-    
-    it('should not create a new folder if that folder already exists', () => {
-      
-    });    
-  });
-  
-  describe('POST /api/v1/links', () => {
-    it('should create a new link', () => {
-      
-    });
-    
-    it('should not create a new link with missing data', () => {
-      
-    });
-  });
-  
-  describe('click events', () => {
-    
-  });
-  
-});
+// });
